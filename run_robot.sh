7c9f1c4299c305e1655e1ddabc47c9f1ac793aca
Aaron Weinstein <adw106@gmail.com>
2017-07-31 15:45:31 -0400

    proper run_robot

--- run_robot.sh
+++ run_robot.sh
@@ -1,47 +1,23 @@
 #!/bin/bash
 
-SESSION_NAME=$USER
-UAV_NAME=$1
+#UAV_NAME=$1
+UAV_NAME="QuadrotorQc11"
 
-if [ -z ${TMUX} ];
-then
-  TMUX= tmux new-session -s $SESSION_NAME -d
-  echo "Starting new session."
-else
-  SESSION_NAME="$(tmux display-message -p '#S')"
-fi
+echo "uav name is ${UAV_NAME}"
 
-echo ${UAV_NAME}
+sleep 1
+echo "stopping snav"
+sudo stop snav; 
 
-# define commands
-# 'name' 'command'
-input=("$USER" "sudo stop snav; sleep 2; sudo start snav; sleep 2; roslaunch state_control_vehicle mav_manager_vio_qc.launch model:=${UAV_NAME}")
+sleep 2
+sudo start snav
 
-# create arrays of names and commands
-for ((i=0; i < ${#input[*]}; i++));
-do
-  ((i%2==0)) && names[$i/2]="${input[$i]}" 
-        ((i%2==1)) && cmds[$i/2]="${input[$i]}"
-done
+sleep 2
+sudo -s << SCRIPT
+source ~/catkin_ws/devel/setup.bash
+roslaunch state_control_vehicle mav_manager_vio_qc.launch model:=${UAV_NAME}
+SCRIPT
 
-# run tmux windows
-for ((i=0; i < ${#names[*]}; i++));
-do
-        tmux new-window -t $SESSION_NAME:$(($i+10)) -n "${names[$i]}"
-done
-
-sleep 4
-
-
-# send commands
-for ((i=0; i < ${#cmds[*]}; i++));
-do
-        tmux send-keys -t $SESSION_NAME:$(($i+10)) "${cmds[$i]}" C-m
-done
-
-sleep 4
-
-tmux select-window -t $SESSION_NAME:1
-tmux -2 attach-session -t $SESSION_NAME
+exit
 
 clear
