a862318c57d064b4add049cb4894e81b504abd84
Aaron Weinstein <adw106@gmail.com>
2017-08-02 14:29:56 -0400

    start tmux session on robot

--- run_robot.sh
+++ run_robot.sh
@@ -4,18 +4,14 @@ UAV_NAME=$1
 
 echo "uav name is ${UAV_NAME}"
 
-sleep 1
-echo "stopping snav"
-sudo stop snav; 
+tmux new-session -s ${UAV_NAME}
+tmux send-keys -t ${UAV_NAME} "sudo stop snav; sleep 2; sudo start snav; sleep 2; roslaunch state_control_vehicle mav_manager_vio_qc.launch model:=${UAV_NAME}" C-m
 
 sleep 2
-sudo start snav
-
-sleep 2
-sudo -s << SCRIPT
-source ~/catkin_ws/devel/setup.bash
-roslaunch state_control_vehicle mav_manager_vio_qc.launch model:=${UAV_NAME}
-SCRIPT
+#sudo -s << SCRIPT
+#source ~/catkin_ws/devel/setup.bash
+#roslaunch state_control_vehicle mav_manager_vio_qc.launch model:=${UAV_NAME}
+#SCRIPT
 
 exit
 
