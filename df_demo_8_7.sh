74abcfcedecf50b512c96f2ea2c59ddc1fc47bf4
Aaron Weinstein <adw106@gmail.com>
2017-08-10 13:43:18 -0400

    increased time delay between start and stop

--- df_demo_8_7.sh
+++ df_demo_8_7.sh
@@ -23,15 +23,15 @@ rosservice call /multi_mav_services/takeoff
 sleep 1
 
 read -p "Press [Enter] to form line"
-rosservice call /multi_mav_services/goFormLine "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [0.0, 0.0, 1.5], spacing: $SPACING}"
+rosservice call /multi_mav_services/goFormLine "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [1.0, 0.0, 1.5], spacing: $SPACING}"
 sleep 1
 
 read -p "Press [Enter] to change to circle"
-rosservice call /multi_mav_services/goFormCircle "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [0.0, 0.0, 1.5], spacing: $SPACING}"
+rosservice call /multi_mav_services/goFormCircle "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [1.0, 0.0, 1.5], spacing: $SPACING}"
 sleep 1
 
 read -p "Press [Enter] to expand circle"
-rosservice call /multi_mav_services/goFormCircle "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [0.0, 0.0, 1.5], spacing: 2.0}"
+rosservice call /multi_mav_services/goFormCircle "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [1.0, 0.0, 1.5], spacing: 2.0}"
 sleep 1
 
 read -p "Press [Enter] to move circle forward"
@@ -39,15 +39,15 @@ rosservice call /multi_mav_services/goFormCircle "{roll: 0.0, pitch: 0.3, yaw: 0
 sleep 1
 
 read -p "Press [Enter] to change to line  backward"
-rosservice call /multi_mav_services/goFormLine "{roll: 0.0, pitch: -0.2, yaw: 0.0, center: [-1.0, 0.0, 1.0], spacing: $SPACING}"
+rosservice call /multi_mav_services/goFormLine "{roll: 0.0, pitch: -0.2, yaw: 0.0, center: [0.0, 0.0, 1.0], spacing: $SPACING}"
 sleep 1
 
 read -p "Press [Enter] to form Rectangle"
-rosservice call /multi_mav_services/goFormRect "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [0.0, 0.0, 1.5], spacing: $SPACING}"
+rosservice call /multi_mav_services/goFormRect "{roll: 0.0, pitch: 0.0, yaw: 0.0, center: [1.0, 0.0, 1.5], spacing: $SPACING}"
 sleep 1
 
 read -p "Press [Enter] to make angled line"
-rosservice call /multi_mav_services/goFormLine "{roll: 0.0, pitch: 0.0, yaw: 1.59, center: [0.0, 0.0, 1.5], spacing: $SPACING}"
+rosservice call /multi_mav_services/goFormLine "{roll: 0.0, pitch: 0.0, yaw: 0.5, center: [1.0, 0.0, 1.5], spacing: $SPACING}"
 sleep 1
 
 read -p "Press [Enter] to form rectangle again"
--- run_robot.sh
+++ run_robot.sh
@@ -8,9 +8,9 @@ echo "uav name is ${UAV_NAME}"
 sudo -s << SCRIPT
 source ~/catkin_ws/devel/setup.bash
 stop snav
-sleep 5
+sleep 7
 start snav
-sleep 5
+sleep 7
 roslaunch state_control_vehicle mav_manager_vio_qc.launch model:=${UAV_NAME}
 SCRIPT
 
